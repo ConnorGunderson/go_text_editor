549170b6dc158ad71e2bce34b9e92a5aa203db60
Connor Gunderson <connor.gunderson@generac.com>
2024-03-19 01:37:39 -0400

    need to fix dogshit creation

--- pkg/models/rope.go
+++ pkg/models/rope.go
@@ -5,7 +5,6 @@ import (
 	"math/big"
 	"math/rand"
 	"strings"
-	"time"
 )
 
 type Rope struct {
@@ -17,60 +16,92 @@ type Rope struct {
 }
 
 // method to find the leaf of the given index
-func (r *Rope) Index(i int) (*Rope, error) {
+func (r *Rope) Index(i int) (*Rope, int, error) {
 
 	// if there is no left or right leaf then then the recursive indexing is at the desired leaf
 	if r.right == nil && r.left == nil {
-		return r, nil
+		return r, i, nil
 	}
 
 	// if the index is greater than the current leaf weight, subtract the weight and traverse right
 	if i > r.weight {
-		r, err := r.right.Index(i - r.weight)
+		r, rIndex, err := r.right.Index(i - r.weight)
 
 		if err != nil {
 			panic(err)
 		}
 
-		return r, nil
+		return r, rIndex, nil
 	}
 
 	// if the index is not greater than the right leaf's weight, use the left leaf
-	r, err := r.left.Index(i)
+	r, lIndex, err := r.left.Index(i)
 
 	if err != nil {
 		panic(err)
 	}
 
-	return r, nil
+	return r, lIndex, nil
 }
 
 // Split the string starting at the index designated
 // re-traverse the tree and subtract the weight and set right-side nodes to nil
-func (r *Rope) Split(index int) (*Rope, string, error) {
-	time.Sleep(500 * time.Millisecond)
-	if index > len(r.Collect()) {
-		return r, "", fmt.Errorf("Index too high")
+func (r *Rope) Split(index int) (leftRope *Rope, ropeStr string, rightRope *Rope, err error) {
+
+	_, ropeStr = r.Collect()
+
+	if index > len(ropeStr) {
+		return r, "", nil, fmt.Errorf("Index too high")
 	}
+
+	rightRopeList := []*Rope{}
+	weightToSub := 0
 
 	// closure to adjust parents weight and remove right node
 	AdjustParentLeaf := func(leaf *Rope) *Rope {
-
 		if leaf.parent != nil {
-			newParentWeight := leaf.parent.weight
-			if leaf.parent.right != nil && leaf.parent.right != leaf && leaf.left != nil {
-				newParentWeight -= leaf.parent.right.weight
+
+			if leaf.parent.right != nil && leaf.parent.right != leaf {
+				rightRopeList = append(rightRopeList, leaf.parent.right)
+				weightToSub += leaf.parent.right.weight
+
+				if leaf.parent.parent != nil && leaf.parent.parent.left == leaf.parent {
+					leaf.parent.parent.weight -= weightToSub
+				}
+
 				leaf.parent.right = nil
 			}
 
-			leaf.parent.weight = newParentWeight
 			return leaf.parent
 		}
 		return leaf
 	}
 
 	// get desired leaf that includes index
-	r, err := r.Index(index)
+	r, leftoverIndex, err := r.Index(index)
+
+	if leftoverIndex > 0 {
+
+		ropeFromLeftovers := CreateRope(leftoverIndex, r.str, r.parent)
+
+		if r == r.parent.left {
+			r.parent.left = ropeFromLeftovers
+		} else {
+			r.parent.right = ropeFromLeftovers
+		}
+
+		r = ropeFromLeftovers
+
+		_, asdf := r.Collect()
+
+		fmt.Println(asdf, r.left, leftoverIndex)
+
+		r, _, err = r.Index(index)
+
+		if err != nil {
+			panic(err)
+		}
+	}
 
 	if err != nil {
 		panic(err)
@@ -80,40 +111,76 @@ func (r *Rope) Split(index int) (*Rope, string, error) {
 		r = AdjustParentLeaf(r)
 
 		if r.parent == nil {
-			return r, r.Collect()[:index], nil
+			// we reconstruct the right rope from the rightRopeList populated by the AdjustParentLeaf closure
+			rightRope, err = RebalanceRope(rightRopeList, 0, len(rightRopeList))
+
+			if err != nil {
+				panic(err)
+			}
+
+			_, ropeStr := r.Collect()
+
+			return r, ropeStr, rightRope, nil
 		}
 	}
 
 }
 
+func RebalanceRope(rl []*Rope, start int, end int) (*Rope, error) {
+
+	if end > len(rl) {
+		return nil, fmt.Errorf("end is greater than the length of the rope list")
+	}
+
+	rnge := end - start
+
+	if rnge == 1 {
+		return rl[0], nil
+	}
+
+	var outRope *Rope
+
+	for _, rope := range rl {
+		if outRope == nil {
+			outRope = rope
+		} else {
+			outRope = outRope.Concat(rope)
+		}
+
+	}
+
+	return outRope, nil
+}
+
 // function to collect all leafs in a rope and return the full string
-func (r *Rope) Collect() string {
+func (r *Rope) Collect() ([]*Rope, string) {
 	rCopy := *r
 
 	ropeStack := []Rope{}
-	ropeList := []Rope{}
+	ropeList := []*Rope{}
 
-	getLeftMostNodeVal := func(rl Rope) {
+	getLeftMostNodeVal := func(rl *Rope) {
 		for {
-			ropeStack = append(ropeStack, rl)
+			ropeStack = append(ropeStack, *rl)
 
 			if rl.left == nil {
 				break
 			}
 
-			rl = *rl.left
+			rl = rl.left
 		}
+
 		ropeList = append(ropeList, rl)
 	}
 
-	getLeftMostNodeVal(rCopy)
+	getLeftMostNodeVal(&rCopy)
 
 	for len(ropeStack) != 0 {
 		parent := ropeStack[len(ropeStack)-1]
 		ropeStack = ropeStack[:len(ropeStack)-1]
 
 		if parent.right != nil {
-			getLeftMostNodeVal(*parent.right)
+			getLeftMostNodeVal(parent.right)
 		}
 	}
 
@@ -123,38 +190,48 @@ func (r *Rope) Collect() string {
 		stringList = append(stringList, ropeList[i].str)
 	}
 
-	return strings.Join(stringList, "")
+	return ropeList, strings.Join(stringList, "")
 }
 
 func (r *Rope) Concat(newR *Rope) *Rope {
+	rLeafList, _ := r.Collect()
 
-	// shallow copy the rope so we don't have to traverse back up when concat-ing a new rope
-	rCopy := *r
 	var newWeight int
 
-	// the new weight of the root node is based on the sum of the child nodes of the left rope
-	// in the instance of this method, we are taking the original rope that is calling the method
-	for {
-		if rCopy.left == nil {
-			if rCopy.parent.left != nil && rCopy.parent.right != nil {
-				leftW := rCopy.parent.left.weight
-				newWeight = leftW
-				break
-			}
-		}
-		rCopy = *rCopy.left
+	for _, leaf := range rLeafList {
+		newWeight += leaf.weight
 	}
 
-	return &Rope{
+	newRope := &Rope{
 		weight: newWeight,
 		right:  newR,
 		left:   r,
 	}
 
+	newRope.right.parent = newRope
+	newRope.left.parent = newRope
+
+	return newRope
+}
+
+func (r *Rope) Insert(idx int, str string) *Rope {
+	left, _, right, err := r.Split(idx)
+
+	if err != nil {
+		panic(err)
+	}
+
+	left = left.Concat(CreateRope(10, str, nil))
+
+	left = left.Concat(right)
+
+	return left
 }
 
 func CreateRope(maxStrLen int, str string, parent *Rope) *Rope {
+
 	if len(str) <= maxStrLen {
+		fmt.Println(str)
 		return &Rope{
 			str:    str,
 			weight: len(str),
@@ -163,6 +240,7 @@ func CreateRope(maxStrLen int, str string, parent *Rope) *Rope {
 	}
 
 	splitNum := len(str) / 2
+	fmt.Println(splitNum)
 
 	str1 := str[:splitNum]
 	str2 := str[splitNum:]
@@ -175,6 +253,7 @@ func CreateRope(maxStrLen int, str string, parent *Rope) *Rope {
 		newRopeLeaf.weight = len(str)
 		newRopeLeaf.left = CreateRope(maxStrLen, str, &newRopeLeaf)
 		newRopeLeaf.right = nil
+
 	} else {
 		newRopeLeaf.weight = len(str1)
 		newRopeLeaf.left = CreateRope(maxStrLen, str1, &newRopeLeaf)
--- pkg/models/rope_test.go
+++ pkg/models/rope_test.go
@@ -1,53 +1,101 @@
 package models
 
 import (
+	"fmt"
 	"testing"
 )
 
 const (
 	START_ROPE_STR_SIZE int = 20000
 	ROPE_TEST_STR           = "Hello_World_I_am_a_Rope!"
+	ROPE_TEST_STR_LEN       = len(ROPE_TEST_STR)
 )
 
-func Test_RopeCollect(t *testing.T) {
+// func Test_RopeCollect(t *testing.T) {
 
-	rope := CreateRandomRope(10, START_ROPE_STR_SIZE)
+// 	rope := CreateRandomRope(10, START_ROPE_STR_SIZE)
 
-	if len(rope.Collect()) != START_ROPE_STR_SIZE {
-		t.Error()
-	}
-}
+// 	_, ropeStr := rope.Collect()
 
-func Test_RopeIndex(t *testing.T) {
+// 	if len(ropeStr) != START_ROPE_STR_SIZE {
+// 		t.Error()
+// 	}
+// }
 
-	rope := CreateRope(4, ROPE_TEST_STR, nil)
-	rope, err := rope.Index(8)
+// func Test_RopeIndex(t *testing.T) {
 
-	if err != nil || rope.str != "Wor" {
-		t.Error()
-	}
-}
+// 	rope := CreateRope(4, ROPE_TEST_STR, nil)
+// 	rope, err := rope.Index(8)
 
-func Test_RopeConcat(t *testing.T) {
+// 	if err != nil || rope.str != "Wor" {
+// 		t.Error()
+// 	}
+// }
 
-	rope := CreateRope(4, ROPE_TEST_STR, nil)
-	rope2 := CreateRope(4, ROPE_TEST_STR, nil)
+// func Test_RopeConcat(t *testing.T) {
 
-	out := rope.Concat(rope2)
+// 	rope := CreateRope(4, ROPE_TEST_STR, nil)
+// 	rope2 := CreateRope(4, ROPE_TEST_STR, nil)
 
-	if out.Collect() != ROPE_TEST_STR+ROPE_TEST_STR {
-		t.Error()
-	}
-}
+// 	out := rope.Concat(rope2)
+
+// 	_, ropeStr := out.Collect()
+
+// 	if out.weight != ROPE_TEST_STR_LEN {
+// 		t.Error("invalid weight")
+// 	}
+
+// 	if ropeStr != ROPE_TEST_STR+ROPE_TEST_STR {
+// 		t.Error("unexpected rope string")
+// 	}
+// }
 
 func Test_RopeSplit(t *testing.T) {
 
 	rope := CreateRope(4, ROPE_TEST_STR, nil)
 
 	// "Hello_World_I_am_a_Rope!"
-	_, newStr, err := rope.Split(20)
+	_, newStr, _, err := rope.Split(2)
 
-	if err != nil || newStr != "Hello_World_I_am_a_R" {
+	fmt.Println(newStr)
+
+	if err != nil || newStr != "Hello_World_I_am_a_Ro" {
 		t.Error()
 	}
 }
+
+// func Test_RopeRebalance(t *testing.T) {
+
+// 	rope1 := CreateRope(4, ROPE_TEST_STR, nil)
+// 	rope2 := CreateRope(7, ROPE_TEST_STR, nil)
+// 	rope3 := CreateRope(3, ROPE_TEST_STR, nil)
+
+// 	ropeList := []*Rope{rope1, rope2, rope3}
+
+// 	rebalancedRope, err := RebalanceRope(ropeList, 0, len(ropeList)-1)
+
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	_, ropeStr := rebalancedRope.Collect()
+
+// 	if ropeStr != "Hello_World_I_am_a_Rope!Hello_World_I_am_a_Rope!Hello_World_I_am_a_Rope!" {
+// 		t.Error("rope not balanced correctly")
+// 	}
+// }
+
+// func Test_RopeInsert(t *testing.T) {
+
+// 	rope1 := CreateRope(4, ROPE_TEST_STR, nil)
+
+// 	newRope := rope1.Insert(2, "INSERT")
+
+// 	_, ropeStr := newRope.Collect()
+
+// 	fmt.Println(ropeStr)
+
+// 	if ropeStr != "HINSERTello_World_I_am_a_Rope!" {
+// 		t.Error("rope not inserted correctly")
+// 	}
+// }
